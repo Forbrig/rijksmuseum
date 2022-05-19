7a7f52d7de227e58e9ea6259c57e3c7e3765fede
Vitor Forbrig <vitorforbrig@gmail.com>
2022-05-18 23:11:49 -0300

    Styled active link on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,24 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+
 import styles from "./styles.module.scss";
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
+        <span>Rijksmuseum</span>
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Search</a>
+          <Link href="/">
+            <a className={asPath === "/" ? styles.active : ""}>Home</a>
+          </Link>
+          <Link href="/collection" prefetch>
+            <a className={asPath === "/collection" ? styles.active : ""}>
+              Collection
+            </a>
+          </Link>
         </nav>
       </div>
     </header>
---
+++ src/pages/collection/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Collection: NextPage = () => {
+  return <div>Collection</div>;
+};
+
+export default Collection;
