90caafa6486642dcc3abf381c63ce5c30e6fe240
Vitor Forbrig <vitorforbrig@gmail.com>
2022-05-22 18:14:02 -0300

    New home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,44 @@
-import type { GetServerSideProps, NextPage } from "next";
+import type { NextPage } from "next";
 import Image from "next/image";
 
-const Home: NextPage = ({ results }: any) => {
+import styles from "./styles.module.scss";
+
+const Home: NextPage = () => {
   return (
-    <div>
-      {results && (
-        <Image
-          src={results.artObjects[0].headerImage.url}
-          alt=""
-          width={600}
-          height={200}
-        />
-      )}
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1>Rijksmuseum</h1>
+
+        <span>
+          This website is a showcase to demonstrate what can be done with the{" "}
+          <a
+            href="https://data.rijksmuseum.nl/object-metadata/api/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rijksmuseum API
+          </a>{" "}
+          and{" "}
+          <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+            NextJS
+          </a>
+          .
+        </span>
+
+        <div className={styles.image}>
+          <div className={styles.imageContainer}>
+            <Image
+              alt="Rijksmuseum picture by Sten Rademaker"
+              src="/rijksmuseum.jpg"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+          <span>Rijksmuseum picture by Sten Rademaker.</span>
+        </div>
+      </div>
     </div>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await fetch(
-    `https://www.rijksmuseum.nl/api/nl/collection?key=${process.env.RIJKMUSEUM_API_KEY}&involvedMaker=Rembrandt+van+Rijn&relevance&ps=1`
-  );
-
-  const data = await response.json();
-
-  console.log(data);
-
-  return {
-    props: {
-      results: data,
-    },
-  };
 };
 
 export default Home;
